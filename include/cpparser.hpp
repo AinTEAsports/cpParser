5cebe50d4b366422c5cb437ca503a66d7c0a7eb7
AinTEAsports <benkirat.khalil@gmail.com>
2022-10-04 21:08:17 +0200

    include/cpparser.hpp: 'ArgumentValue' is now convertible with 'bool()', 'std::string()' and 'std::vector<std::string>()'

--- include/cpparser.hpp
+++ include/cpparser.hpp
@@ -85,6 +85,11 @@ struct ArgumentValue {
     std::string String = "";
     std::vector<std::string> Vector = {};
     bool Bool;
+
+
+    operator std::string() {return String;}
+    operator std::vector<std::string>() {return Vector;}
+    operator bool() {return Bool;}
 };
 
 std::ostream& operator<<(std::ostream &stream, const ArgumentValue &argval);
